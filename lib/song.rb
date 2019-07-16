4caa6b68f71e9b8e407c0aef7fe6b5425def5a74
Sean Murphy <filmmakersean@fastmail.net>
2019-07-16 23:28:19 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -29,9 +29,9 @@ class Song
     tune
   end
   
-  def self.find_by_name(name)
-    @@all.each do |song|
-      if song.name == name
+  def self.find_by_name(title)
+    Song.all.each do |song|
+      if song.name == title
         song
       end
     end
